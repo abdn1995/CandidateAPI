9ffbe71faa5c5ded9e9f7bd75116d71f0b3e833f
Abd Ar Rhman Nagy <abdarrhmannagy@gmail.com>
2024-05-12 01:20:29 +0300

    Candidate Controller
    Implemented the candidate controller to be used for the API Calls
    
    The controller Implements GET - POST - PUT - DELETE Calls

---
+++ CandidateAPI/Controllers/CandidatesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandidateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+    }
+}
