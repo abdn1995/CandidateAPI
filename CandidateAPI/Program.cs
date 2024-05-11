d5840bfc98fbbc6ebe9c78c04842e3d3768f8b24
Abd Ar Rhman Nagy <abdarrhmannagy@gmail.com>
2024-05-12 01:06:48 +0300

    Update Program.cs
    Added the memory cache to be used for the API caching

--- CandidateAPI/Program.cs
+++ CandidateAPI/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CandidateContext")));
 
+builder.Services.AddMemoryCache();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
